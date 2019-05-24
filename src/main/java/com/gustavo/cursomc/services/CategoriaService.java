ea50585aed9dd2132a992ad0305096f250cdee7c
Gustavo Faria <gustavo.faria@totvs.com.br>
2019-05-24 10:52:05 -0300

    criando controle de exceções no endpoint /categorias

--- src/main/java/com/gustavo/cursomc/services/CategoriaService.java
+++ src/main/java/com/gustavo/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.gustavo.cursomc.services;
 
 import com.gustavo.cursomc.domain.Categoria;
 import com.gustavo.cursomc.repositories.CategoriaRepository;
+import com.gustavo.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,11 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id) {
         Categoria obj = repo.findOne(id);
+
+        if(obj == null) {
+            throw new ObjectNotFoundException("Objeto com Id: " + id + " não encontrado!"
+                                        + "Tipo: " + Categoria.class.getName());
+        }
         return obj;
     }
 }
